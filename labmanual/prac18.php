30815ed6cf74fb0eb4e8ce13d1818f4a9075e5f0
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-09-11 21:49:44 +0530

    added all  practical codes

---
+++ labmanual/prac18.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>PERSONAL DETAILS</h2>";
+$name = "Sai kiran Voladri";
+$age = 20;
+$university = "PU";
+echo "<h2>Hello everyone 👋</h2>";
+echo "My name is <strong>$name
+    </strong> and I am $age years old. 
+    I am studying in <i>$university.</i>";
+?>
+
