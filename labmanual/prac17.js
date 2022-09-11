4451a108170f3fa82fb0245db1f329b2c5a3c0fb
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-09-11 21:54:54 +0530

    Update prac17.js

--- labmanual/prac17.js
+++ labmanual/prac17.js
@@ -1,6 +1,6 @@
-function parseJson() {
-    var content = '{ "students": [ { "name": "Sai", "age": "20", "marks": [4, 5, 5] }, { "name": "Kiran", "age": "21", "marks": [5, 5, 5] }, { "name": "Mani", "age": "22", "marks": [5, 5, 5] } ]}';
-    var json = JSON.parse(content);
+//function parseJson() {
+   // var content = '{ "students": [ { "name": "Sai", "age": "20", "marks": [4, 5, 5] }, { "name": "Kiran", "age": "21", "marks": [5, 5, 5] }, { "name": "Mani", "age": "22", "marks": [5, 5, 5] } ]}';
+   // var json = JSON.parse(content);
     // show on document
-    document.getElementById('json').innerHTML = json.students[0].name;
-}
+  //  document.getElementById('json').innerHTML = json.students[0].name;
+//}
